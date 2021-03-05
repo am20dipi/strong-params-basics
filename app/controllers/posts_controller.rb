77c81dce350959f78bab0fc2b0e99143f137e5e5
Adriana <am20dipi@alum.siena.edu>
2021-03-04 20:25:34 -0500

    commit tree 8fac7712d37d59428a29b3f8b02cd92e629fd240

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,16 +12,19 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new(params["post"])
+	  @post = Post.new(params.require(:post).permit(:title, :description))
 	  @post.save
 	  redirect_to post_path(@post)
 	end
 
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(params["post"])
+	  @post.update(params.require(:post).permit(:title))
 	  redirect_to post_path(@post)
 	end
+
+	# require: the require method means that the params that get passed in must contain a key called "post"
+	# permit: the permit methods means that the params hash may have whatever keys are called in the create/update method.
 
 	def edit
 	  @post = Post.find(params[:id])
